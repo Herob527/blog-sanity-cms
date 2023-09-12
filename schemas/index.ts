afddcf788dc274306ab60a5e386be66a5d52d5db
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-09-12 11:01:12 +0200

    feat: Added stack entry schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import stackEntry from './stackEntry'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, stackEntry]
